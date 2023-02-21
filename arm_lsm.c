bfd4872e437638d616b9a181d3feb4f0b74d29a8
yzc <15773946499@163.com>
2023-02-21 20:28:58 +0800

    add check_mmu()
    Signed-off-by: yzc <15773946499@163.com>

--- arm_lsm.c
+++ arm_lsm.c
@@ -14,8 +14,6 @@
 #include <linux/mm.h>
 #include <linux/kallsyms.h>
 
-int ininitrd = 0;
-int platform_status=0;
 
 struct security_hook_heads *my_hook_head;
 
@@ -25,173 +23,40 @@ struct
 	unsigned int addr; // 高32位为idt表地址
 }__attribute__((packed)) idtr; // idtr是48位6字节寄存器
 
-typedef struct os_security {
-    int indicate;              // indicate if the proc to monitor
-} os_security_t;
-
-struct super_proc{
-	char *install_proc;
-	pid_t super_pid;	//root proc pid
-	pid_t install_pid;
-//	pid_t lastfound;
-	unsigned long checksum;
-};
-
-struct super_proc root_proc = {
-	.install_proc	= "os_install",
-	.install_pid	= 1,
-	.checksum	= 1,//exec permission checksum,exec permission expire when "os_install" proc exit
-	.super_pid	= 1, //os_install's parent proc ip,"bash"
-};
-
 unsigned long clear_and_return_cr0(void);
 void setback_cr0(unsigned long val);
 void my_init_security_hook_list(void);
 static void my_add_hooks(struct security_hook_list *hooks, int count, char *lsm);
 static void my_del_hooks(struct security_hook_list *hooks, int count);
 
-// unsigned long clear_and_return_cr0()
-// {
-// 	unsigned long cr0 = 0;
-// 	unsigned long ret;
-// 	asm volatile("movq %%cr0,%%rax"
-// 				 : "=a"(cr0));
-// 	ret = cr0;
-// 	cr0 &= 0xfffeffff;
-// 	asm volatile("movq %%rax,%%cr0" ::"a"(cr0));
-// 	return ret;
-// }
-
-// void setback_cr0(unsigned long val)
-// {
-// 	asm volatile("movq %%rax,%%cr0" ::"a"(val));
-// }
 
 int my_file_open(struct file *file)
 {
-	printk("The file opened is %s\n", file->f_path.dentry->d_iname);
-	return 0;
-}
-
-void
-super_proc_init(void)
-{
-
-	struct task_struct *p;
-
-	if (root_proc.super_pid != 1)
-		return;
-	p = current;
-	while(p->parent->pid != 1)
-	{
-//		DEBUG("super_proc loading ,p [%s]\n",p->comm);
-		if(!strcmp(p->parent->comm,"SecurityManage"))
-		{
-			root_proc.super_pid= p->parent->pid;
-			root_proc.install_pid = p->pid;
-			printk("super_proc load success,pid [%d]\n",root_proc.super_pid);
-		}
-		p = p->parent;
+	if(file != NULL){
+		//printk("The file opened is %s\n", file->f_path.dentry->d_iname);
 	}
-	return;
-}
-
-int super_proc_clean(void)
-{
-//DEBUG("super_proc unload [%d]\n",root_proc.super_pid);
-	root_proc.super_pid = 1;
-	root_proc.install_pid = 1;
-	return 0;
-}
-
-
-int os_fs_bprm_alloc_security(struct linux_binprm *bprm)
-{
-//	struct task_struct *iterate_task = current;
-
-	if(!strcmp(bprm->filename,"/boot/securitymanage/SecurityManage"))
-		{
-		super_proc_clean();
-		return 0;
-		}
-
-	if(root_proc.super_pid != 1)
-		{
-		return 0;
-		}
-	
-	if (!strcmp(current->comm,"os_install"))
-	{
-		if(current->parent->parent==NULL)
-			return 0;
-		if(((!strcmp(current->parent->parent->comm,"xterm"))&&(!strcmp(current->parent->parent->parent->comm,"SecurityManage")))||
-				(!strcmp(current->parent->parent->comm,"SecurityManage")))
-			{
-			super_proc_init();
-			return 0;
-			}
-		if(current->parent->parent->parent==NULL)
-			return 0;
-		if((!strcmp(current->parent->parent->comm,"xterm"))&&(!strcmp(current->parent->parent->parent->comm,"SecurityManage")))
-			{
-			super_proc_init();
-			return 0;
-			}		
-	}
-
-/*
-	if (!strcmp(bprm->filename,"/bin/os_install"))
-	{
-		for (;iterate_task->pid != 1;iterate_task = iterate_task->parent )
-			{
-			if(!strcmp(iterate_task->comm,"SecurityManage"))
-				{
-				super_proc_init();
-				return 0;
-				}
-			}
-	}
-*/
-
+		
 	return 0;
 }
 
 
 int my_bprm_check_security(struct linux_binprm *bprm)
 {
-	if(ininitrd)
-	{
-	if(!memcmp(bprm->filename,"/sbin/init",10))
-		{
-		platform_status = ininitrd;
-		ininitrd = 0;
-		printk("tjsdebug: Get out of initrd here?\n\n");
-		}
-	}
+
 	if(bprm != NULL){
-		printk("bprm file  %s\n", bprm->file->f_path.dentry->d_iname);
+		//printk("bprm file  %s\n", bprm->file->f_path.dentry->d_iname);
 	}
-	return os_fs_bprm_alloc_security(bprm);
-    //return os_fs_bprm_check_security (bprm);
-	//return 0;
-}
 
-
-int my_kernel_module_request(char *kmod_name)
-{
-	if(kmod_name != NULL){
-		printk("User add module  {%s}\n", kmod_name);
-	}
-	printk("User add module \n");
 	return 0;
 }
+
 
 int my_kernel_read_file(struct file *file, enum kernel_read_file_id id,
 			      bool contents)
 {
 	if(file != NULL){
-		printk("User add module file pointer {%p}\n",  file); 
-		printk("User add module {%s}\n",  file->f_path.dentry->d_name.name); 
+		//printk("User add module file pointer {%p}\n",  file); 
+		//printk("User add module {%s}\n",  file->f_path.dentry->d_name.name); 
 	}
 	
 	return 0;
@@ -199,27 +64,10 @@ int my_kernel_read_file(struct file *file, enum kernel_read_file_id id,
 
 int my_mmap_file(struct file *file, unsigned long reqprot, unsigned long prot, unsigned long flags)
 {
-	// static const char proc_name[] = "proc";
-	// os_security_t *sec = (os_security_t *)current->cred->security;  //current宏指向当前内核正在执行的进程的结构体
-	// if (file != NULL ) {
-	// 	//struct passwd *pwd = getpwuid(getuid());
-	// 	printk("Process {%s} mmap file {%s}\n", proc_name, file->f_path.dentry->d_name.name);
-	// 	// printk("Process {%s} mmap file {%llx}\n", proc_name, file);
-	// 	//return 0;
-	// }
-	// if (file != NULL && sec != NULL && sec->indicate == 1) {  //匿名映射
-	// 	printk("Process {%s} mmap file {%s}\n", proc_name, file->f_path.dentry->d_name.name);   //打印文件名字，而不是整个路径，
-	// 	// printk("Process {%s} mmap file {%llx}\n", proc_name, file);
-	// }
-
-	os_security_t *sec = (os_security_t *)current->cred->security;
 	if(file != NULL)
 	{
-//		printk("Process mmap file {%s}\n",  file->f_path.dentry->d_name.name); 
+		//printk("Process mmap file {%s}\n",  file->f_path.dentry->d_name.name); 
 	}
-	//printk("Process mmap file {}\n");  //未oops
-	//printk("Process mmap file {file pointer :0x%p}\n",  file);  //oops
-	//printk("Process mmap file {f_path.dentry pointer :0x%p}\n",  file->f_path.dentry);  //oops
 	
 	return 0;
 }
@@ -228,29 +76,48 @@ int check_syscall(void)
 {
 	unsigned long *sys_call_table = 0;
 	sys_call_table = (unsigned long *)kallsyms_lookup_name("sys_call_table");
-	//printk("sys_call_table_addr:%lx \n"); 
+	printk("sys_call_table_addr:%lx \n"); 
 
 	return 0;  
 }
 
-// int check_idt(void)
-// {
-// 	asm("sidt %0":"=m"(idtr)); 
-// 	printk(KERN_ALERT "idt table adr: 0x%x\n", idtr.addr);
+int check_mmu(void)
+{
+	int kernel_pagetable_base, user_pagetable_base, sctlr_el1=0;
 
-// 	return 0;  
-// }
+	asm volatile("mrs %0, TTBR1_EL1" 
+		: "=r" (kernel_pagetable_base)
+	);
+	printk("kernel_pagetable_base: 0x%x\n", kernel_pagetable_base);
+	// asm volatile("mrs %0, TTBR0_EL0" 
+	// 	: "=r" (user_pagetable_base)
+	// );
+	// printk("user_pagetable_base: 0x%x\n", user_pagetable_base);
+
+	asm volatile("mrs %0, SCTLR_EL1" //读sctlr系统寄存器
+		: "=r" (sctlr_el1)
+	);
+	printk("sctlr_el1: 0x%x\n", sctlr_el1);
+
+	return 0;  
+}
+
+int check_idt(void)
+{
+	return 0;
+}
 
 int my_task_alloc(struct task_struct *task,unsigned long clone_flags)
 {
-	check_syscall();
+	//check_syscall();
+	check_mmu();
 	//check_idt();
-//    printk("[+geek] call task_create().\n");    
+
     return 0;
 }
 
 
-struct security_hook_list hooks[2]; //目的是将这两个security_hook_list结构体中的list head指针插入到security_hook_heads结构体中对应的位置
+struct security_hook_list hooks[]; //目的是将这两个security_hook_list结构体中的list head指针插入到security_hook_heads结构体中对应的位置
 
 void my_init_security_hook_list(void)
 {
